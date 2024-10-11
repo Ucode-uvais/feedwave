221b3a0af5f4c20f919a6b4282d8ae2318936c37
SEYYED UVAIS <156394395+Ucode-uvais@users.noreply.github.com>
2024-10-11 16:00:08 +0530

    Update page.tsx

--- app/(user)/projects/[projectId]/instructions/page.tsx
+++ app/(user)/projects/[projectId]/instructions/page.tsx
@@ -7,24 +7,25 @@ const Page = ({
     projectId: string;
   };
 }) => {
-  if (!params.projectId) return <div>Invalid Project Id</div>;
+  if (!params.projectId) return <div>Invalid Project ID</div>;
   if (!process.env.WIDGET_URL) return <div>Missing WIDGET_URL</div>;
+
   return (
     <div>
       <h1 className="text-xl font-bold mb-2">Start Collecting Feedback</h1>
       <p className="text-lg text-secondary-foreground">
         Embed the code in your site
       </p>
-      <pre className="bg-purple-900 p-6 rounded-md mt-6 relative">
-        <CopyBtn
-          text={`<my-widget project="${params.projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
-        />
-        <code className="text-white">
+      <div className="bg-blue-950 p-6 rounded-md mt-6 relative">
+        <code className=" text-white">
           {`<my-widget project-id="${params.projectId}"></my-widget>`}
           <br />
           {`<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
         </code>
-      </pre>
+        <CopyBtn
+          text={`<my-widget project-id="${params.projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
+        />
+      </div>
     </div>
   );
 };
